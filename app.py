6861705594ddd2469542e11fad1c133dfe7a1a8b
Nibedan Bhattacharjee <148060647+nibedanbhattacharjee97@users.noreply.github.com>
2024-02-15 11:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+
 st.set_page_config("Anudip Placement Chatbot", page_icon=":book")
 
 def load_data():
@@ -18,7 +19,7 @@ def get_answer(question, data):
     else:
         return "I'm sorry, I don't know the answer to that question.", None
 
-def main():
+def Placement():
     st.title("Placement Documents Chatbot")
 
     # Load data from CSV
@@ -40,5 +41,53 @@ def main():
         else:
             st.warning("No picture available for this answer.")
 
+def load_dataen():
+    # Load the CSV file
+    df = pd.read_csv("dataen.csv")
+    return df
+
+def get_answer(question, data):
+    # Find the corresponding answer for the given question
+    answer_row = data[data['Question'] == question]
+    if not answer_row.empty:
+        answer = answer_row.iloc[0]['Answer']
+        picture_path = answer_row.iloc[0]['PicturePath']
+        return answer, picture_path
+    else:
+        return "I'm sorry, I don't know the answer to that question.", None
+
+
+def Enrollment():
+    st.title("Enrollment Documents Chatbot")
+
+    # Load data from CSV
+    data = load_dataen()
+
+    # User input
+    user_question = st.selectbox("Ask a question:", data['Question'].unique())
+
+    if st.button("Get Answer"):
+        # Get the answer and picture path
+        answer, picture_path = get_answer(user_question, data)
+
+        # Display the answer
+        st.markdown(f"**Answer:** {answer}")
+
+        # Display the picture if available
+        if picture_path:
+            st.image(picture_path, caption='Answer Image', use_column_width=True)
+        else:
+            st.warning("No picture available for this answer.")
+
+
 if __name__ == "__main__":
-    main()
+     pages = {
+        "Placement": Placement,
+        "Enrollment": Enrollment,
+    }
+st.sidebar.title('Navigation')
+selection = st.sidebar.selectbox("Go to", list(pages.keys()))
+
+page = pages[selection]
+page()
+
